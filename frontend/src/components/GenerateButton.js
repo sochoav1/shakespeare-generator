73e2e83995bf3550f8f0d2092a19fc4bb737ccd4
Santiago Ochoa <sochoav8a@gmail.com>
2024-07-01 02:23:40 -0500

    add images

--- frontend/src/components/GenerateButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const GenerateButton = ({ onClick, loading }) => (
-    <button onClick={onClick} disabled={loading}>
-        {loading ? 'Generating...' : 'Generate'}
-    </button>
-);
-
-export default GenerateButton;
